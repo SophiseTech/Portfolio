fee5f2ff4eddd42670bc3fb42f05325920efb894
aarsha01 <malu22leena@gmail.com>
2024-10-01 17:46:05 +0530

    changes in button color

--- app/Components/NavBar/Main.jsx
+++ app/Components/NavBar/Main.jsx
@@ -1,22 +1,23 @@
+
 function NavBar() {
   const ListItem=({name})=>{
     return (
-      <li className="py-2 px-5 mx-2 my-2 hover:backdrop-blur-sm hover:bg-white/5 hover:rounded-full hover:shadow-lg hover:border-1">
+      <li className="py-2 px-5 mx-2 my-2 text-gray-300 hover:text-white hover:backdrop-blur-sm hover:bg-white/10 hover:rounded-full hover:shadow-lg hover:border-1">
         {name}
       </li>
     );
   };
   const SigningButtons=({names})=>{
     return(
-      <button className="mx-2 bg-transparent border-2 border-[#ff48bb] hover:bg-[#ff48bb] text-white px-5 py-2 rounded-full">{names}</button>
+      <button className="mx-2 hover:text-black text-blackbg-transparent border-2 border-white hover:bg-white px-5 py-2 rounded-full">{names}</button>
     )
   }
   return (
     <div className=" w-full flex justify-center   fixed top-0 z-50">
       <div className="flex justify-between items-center w-11/12">
-        <div  className="text-3xl font-semibold p-5 ">Nyon</div>
+        <div  className=" text-3xl font-semibold p-5 ">Nyon</div>
         <div >
-            <ul className="flex backdrop-blur-lg  bg-white/5 rounded-full shadow-lg shadow-white/5 border-1 my-2 px-3  ">
+            <ul className="flex bg-white/10 rounded-full  bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-50  border-stone-600 border-2 my-2 px-3  ">
             
                <ListItem name="Home"/>
                <ListItem name="About"/>
@@ -24,9 +25,10 @@ function NavBar() {
                <ListItem name="Project"/>
             </ul>
         </div>
-        <div className="last-element p-3">
-            <SigningButtons names="Sign Up"/>
-            <SigningButtons names="Login"/>
+        <div className="last-element p-3 ">
+        <button className="mx-2 text-black hover:text-white hover:bg-transparent border-2 border-white bg-white px-5 py-2 rounded-full">Sign Up</button>
+        <button className="mx-2 hover:text-black text-blackbg-transparent border-2 border-white hover:bg-white px-5 py-2 rounded-full">Login</button>
+
          
         </div>
         
@@ -36,5 +38,6 @@ function NavBar() {
   )
 
 }
+
 
 export default NavBar
