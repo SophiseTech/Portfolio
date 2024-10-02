4bf75fa4eb8ad07ec3bd6a9aaa3a9a3977882d2b
aarsha01 <malu22leena@gmail.com>
2024-10-02 18:27:27 +0530

    finished navbar section

--- app/Components/NavBar/Main.jsx
+++ app/Components/NavBar/Main.jsx
@@ -13,11 +13,11 @@ function NavBar() {
     )
   }
   return (
-    <div className=" w-full flex justify-center   fixed top-0 z-50">
-      <div className="flex justify-between items-center w-11/12">
-        <div  className=" text-3xl font-semibold p-5 ">Nyon</div>
+    <div className=" w-full flex justify-center fixed top-0 z-50">
+      <div className="grid grid-cols-3 items-center w-11/12">
+        <div  className=" text-3xl font-semibold p-5  justify-self-start">Nyon</div>
         <div >
-            <ul className="flex bg-white/10 rounded-full  bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-50  border-stone-600 border-2 my-2 px-3  ">
+            <ul className="flex justify-center bg-white/10 rounded-full  bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-50  border-stone-600 border-2 my-2 px-3  ">
             
                <ListItem name="Home"/>
                <ListItem name="About"/>
@@ -25,7 +25,7 @@ function NavBar() {
                <ListItem name="Project"/>
             </ul>
         </div>
-        <div className="last-element p-3 ">
+        <div className="last-element p-3 justify-self-end">
         <button className="mx-2 text-black hover:text-white hover:bg-transparent border-2 border-white bg-white px-5 py-2 rounded-full">Sign Up</button>
         <button className="mx-2 hover:text-black text-blackbg-transparent border-2 border-white hover:bg-white px-5 py-2 rounded-full">Login</button>
 
