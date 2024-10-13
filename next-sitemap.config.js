43dc5b3e70bff0c95a1d6c1142d2f29fb8e00ddd
SophiseTech <sophisetech@gmail.com>
2024-10-14 01:21:49 +0530

    FAB name change to fab, seo content added

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl:  'https://nyontech.vercel.app' || 'https://nyon.in', // Your website URL
+  generateRobotsTxt: true, // Optionally generate a `robots.txt` file
+};
