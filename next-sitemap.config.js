f111d1560092d20efc7b9b67e14690ecd7228b7c
jazzirHussain <jasirhussain2002@gmail.com>
2025-02-18 01:40:18 +0530

    changer next.sitemap.congif

--- next-sitemap.config.js
+++
@@ -1,5 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl:  'https://nyontech.vercel.app' || 'https://nyon.in', // Your website URL
-  generateRobotsTxt: true, // Optionally generate a `robots.txt` file
-};
